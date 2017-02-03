3e4c18aca7bd203b4cad40430c075d85584ee101
David <C00221536@itcarlow.ie>
2017-02-03 13:32:55 +0100

    Add redireection About

--- app/src/main/java/com/example/david/webapp/about.java
+++ app/src/main/java/com/example/david/webapp/about.java
@@ -1,7 +1,11 @@
 package com.example.david.webapp;
 
+import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class about extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class about extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+    }
+
+    public void itCarlow (View view) {
+        Intent itC = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.itcarlow.ie/")); //Redirect link to IT Carlow website
+        startActivity(itC); //Start of the Activity
+    }
+
+    public void uB(View view){
+        Intent ub = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.u-bourgogne.fr/")); //Redirect link to UB
+        startActivity(ub); //Start the activity
+    }
+    public void ubfc(View view){
+        Intent ubfc = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.ubfc.fr/")); //Redirect UBFC
+        startActivity(ubfc); //Start activity
     }
 }

6451bc86da803d84d3acf769c6b9139589d2da21
David <C00221536@itcarlow.ie>
2017-02-03 16:38:14 +0100

    Add Layout for Employement + code (Class employement)

--- app/src/main/java/com/example/david/webapp/employement.java
+++ app/src/main/java/com/example/david/webapp/employement.java
@@ -88,45 +88,6 @@ public class employement extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_employement, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         public SectionsPagerAdapter(FragmentManager fm) {
@@ -135,9 +96,16 @@ public class employement extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            switch (position){
+                case 0:
+                    return new employementtab1();
+                case 1:
+                    return new employementtab2();
+                case 2:
+                    return new employementtab3();
+                default:
+                    return null;
+            }
         }
 
         @Override
@@ -150,11 +118,11 @@ public class employement extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "Student Job";
                 case 1:
-                    return "SECTION 2";
+                    return "Placement";
                 case 2:
-                    return "SECTION 3";
+                    return "Placement";
             }
             return null;
         }
---
+++ app/src/main/java/com/example/david/webapp/employementtab1.java
@@ -0,0 +1,20 @@
+package com.example.david.webapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by David on 03/02/2017.
+ */
+
+public class employementtab1 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.employementtab1, container, false);
+        return rootView;
+    }
+}
